6011dcef88527a1f4bc0fc1792663fcb2c79f906
hmel1990 <hmel408757595@gmail.com>
2025-06-17 10:31:32 +0300

    useState

--- src/components/modern/Modern.jsx
+++ src/components/modern/Modern.jsx
@@ -7,7 +7,7 @@ export default function Modern ()
         <>
             <div className="wrapper">
                 <h2>Современная Одесса</h2>
-                <div className="image"> <img src="https://raw.githubusercontent.com/hmel1990/ProfilePhoto/refs/heads/main/licensed-image%20(2).jfif"/> </div>
+                <div className="image"> <img alt="Одесса" src="https://raw.githubusercontent.com/hmel1990/ProfilePhoto/refs/heads/main/licensed-image%20(2).jfif"/> </div>
                 <p className="text"> Сегодня Одесса — это современный город с развитой инфраструктурой, университетами, музеями и активной культурной жизнью.
                     Несмотря на все вызовы времени, она сохраняет своё очарование и открытость, продолжая вдохновлять, удивлять и влюблять в себя с первого взгляда.</p>
 
